89c12693c2fde92112f7e1334b6d984bf700dea2
Evgeniy Smirnov <esmirnov@ostrovok.ru>
2019-12-07 18:11:45 +0300

    build frontend

---
+++ frontend/build/precache-manifest.2fa388951a2caf12519de5279bd99797.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b12c978913358777b96c6366152f115",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e045d6bdef99001621e9",
+    "url": "/static/css/2.15f90017.chunk.css"
+  },
+  {
+    "revision": "e07984b918582b3e533c",
+    "url": "/static/css/main.dd07a0b2.chunk.css"
+  },
+  {
+    "revision": "e045d6bdef99001621e9",
+    "url": "/static/js/2.611ddfc0.chunk.js"
+  },
+  {
+    "revision": "bbafffd26e2c01828bf791cfc395af61",
+    "url": "/static/js/2.611ddfc0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e07984b918582b3e533c",
+    "url": "/static/js/main.a8ec4b52.chunk.js"
+  },
+  {
+    "revision": "a41ace8688e58ddcaf3c",
+    "url": "/static/js/runtime-main.eecceb69.js"
+  },
+  {
+    "revision": "f9ed6839e6de1e82031ed869747cb708",
+    "url": "/static/media/logo.f9ed6839.svg"
+  }
+]);
