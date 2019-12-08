15a9bf5830824b9788457fe82eecfdbb13bd416b
GeorgeGUS27 <george.gusakov@ostrovok.ru>
2019-12-08 11:04:45 +0300

    Add support phone number

---
+++ frontend/src/components/SupportPhoneNumber/SupportPhoneNumber.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Icon, Typography } from 'antd';
+
+import './SupportPhoneNumber.css';
+
+const { Text } = Typography;
+
+const SupportPhoneNumber = () => (
+  <div className="support">
+    <Text>Позвоните нам, если хотите оставить заявку по телефону</Text>
+    <a href="tel:8 800 505 04 03" className="phone-number">
+      <Icon type="phone" theme="filled" /> 8 800 505 04 03
+    </a>
+  </div>
+);
+
+export default SupportPhoneNumber;
