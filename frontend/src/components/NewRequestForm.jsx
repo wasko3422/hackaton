fbd7f1c4d71191b671cec8b93bef7abf52300824
GeorgeGUS27 <george.gusakov@ostrovok.ru>
2019-12-08 09:55:02 +0300

    Remove required from dealerId field

--- frontend/src/components/NewRequestForm.jsx
+++ frontend/src/components/NewRequestForm.jsx
@@ -117,14 +117,7 @@ class NewRequestForm extends Component {
         </Form.Item>
         {isMapShown && (
           <Form.Item wrapperCol={{ span: 24 }} label="Выберите дилера">
-            {getFieldDecorator('dealerId', {
-              rules: [
-                {
-                  required: true,
-                  message: 'Обязательное поле',
-                },
-              ],
-            })(<DealersMap />)}
+            {getFieldDecorator('dealerId')(<DealersMap />)}
           </Form.Item>
         )}
         <Form.Item label="Укажите текущий пробег автомобиля">
